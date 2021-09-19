f132803e4e84f56da2621cd2d91f646c9bdcef03
Corey Runkel <runkelcorey@gmail.com>
2021-09-19 19:45:03 -0400

    Added user agent

--- R/valet.R
+++ R/valet.R
@@ -1,7 +1,7 @@
 valet <- function(name = NULL, group = F) {
   url <- modify_url("https://www.bankofcanada.ca/", path = paste0("valet/observations/", ifelse(group, "group/", ""), name, "/json"))
 
-  resp <- GET(url)
+  resp <- GET(url, user_agent("https://github.com/runkelcorey/valet"))
 
   parsed <- jsonlite::fromJSON(content(resp, "text"))
 
